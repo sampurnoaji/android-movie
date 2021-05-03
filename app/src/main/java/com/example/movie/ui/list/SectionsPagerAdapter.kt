53b361cd1545a6baecef64761867d07961dc63d2
petersam <man.sanji23@gmail.com>
2021-05-03 15:47:27 +0700

    init tab layout

---
+++ app/src/main/java/com/example/movie/ui/list/SectionsPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.movie.ui.list
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.movie.R
+import com.example.movie.ui.list.movie.MoviesFragment
+import com.example.movie.ui.list.show.ShowsFragment
+
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.movies, R.string.shows)
+    }
+
+    override fun getItem(position: Int): Fragment =
+        when (position) {
+            0 -> MoviesFragment()
+            1 -> ShowsFragment()
+            else -> Fragment()
+        }
+
+    override fun getPageTitle(position: Int): CharSequence = mContext.resources.getString(TAB_TITLES[position])
+
+    override fun getCount(): Int = 2
+
+}
