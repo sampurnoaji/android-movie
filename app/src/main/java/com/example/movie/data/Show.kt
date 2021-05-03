057cfb684391efd996d2ef8b6475aecac1e31ad5
petersam <man.sanji23@gmail.com>
2021-05-03 17:23:53 +0700

    finish tv show list

---
+++ app/src/main/java/com/example/movie/data/Show.kt
@@ -0,0 +1,9 @@
+package com.example.movie.data
+
+data class Show(
+    val id: Int,
+    val title: String,
+    val releaseDate: String,
+    val overview: String,
+    val posterUrl: String
+)
--- app/src/main/java/com/example/movie/ui/list/show/ShowsFragment.kt
+++ app/src/main/java/com/example/movie/ui/list/show/ShowsFragment.kt
@@ -1,60 +1,39 @@
 package com.example.movie.ui.list.show
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.movie.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movie.databinding.FragmentShowsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ShowsFragment : Fragment(), ShowListAdapter.ShowItemListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ShowsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ShowsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentShowsBinding
+    private val vm: ShowsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shows, container, false)
+    ): View {
+        binding = FragmentShowsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShowsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShowsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val showListAdapter = ShowListAdapter(vm.getShows(), this)
+        with(binding.rvMovies) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = showListAdapter
+        }
+    }
+
+    override fun onShowClicked(id: Int) {
+
     }
 }
