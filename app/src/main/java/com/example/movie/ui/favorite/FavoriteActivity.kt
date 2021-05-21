151e6fca9b7ef6ecd91627d49347bb3034663fb5
petersam <man.sanji23@gmail.com>
2021-05-21 22:31:56 +0700

    init blank FavoriteActivity

---
+++ app/src/main/java/com/example/movie/ui/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movie.ui.favorite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movie.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
--- app/src/main/java/com/example/movie/ui/list/MainActivity.kt
+++ app/src/main/java/com/example/movie/ui/list/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.movie.ui.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.movie.R
 import com.example.movie.databinding.ActivityMainBinding
+import com.example.movie.ui.favorite.FavoriteActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,22 @@ class MainActivity : AppCompatActivity() {
         binding.tabs.setupWithViewPager(binding.viewPager)
 
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_favorite -> {
+                val intent = Intent(this, FavoriteActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.action_newest -> Toast.makeText(this, "new", Toast.LENGTH_SHORT).show()
+            R.id.action_oldest -> Toast.makeText(this, "old", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
