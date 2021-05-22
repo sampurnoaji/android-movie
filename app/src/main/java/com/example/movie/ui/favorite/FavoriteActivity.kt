cec9896fe6a55c5d710b08ae2a1a8c6a1e1a8a9f
petersam <man.sanji23@gmail.com>
2021-05-22 18:44:01 +0700

    fix onbackpressed

--- app/src/main/java/com/example/movie/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/example/movie/ui/favorite/FavoriteActivity.kt
@@ -1,9 +1,6 @@
 package com.example.movie.ui.favorite
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.movie.R
@@ -31,18 +28,6 @@ class FavoriteActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_favorite, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_newest -> Toast.makeText(this, "new", Toast.LENGTH_SHORT).show()
-            R.id.action_oldest -> Toast.makeText(this, "old", Toast.LENGTH_SHORT).show()
-        }
-        return super.onOptionsItemSelected(item)
+        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }
